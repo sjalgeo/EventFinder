f3985b9ff397da723d5b724a1d6b54e990750258
Sapsford11 <craig.sapsford11@gmail.com>
2018-05-03 11:11:44 +0200

    Updates

--- src/components/util/EventBrite.js
+++ src/components/util/EventBrite.js
@@ -3,7 +3,7 @@ const myToken = '2OHPMQAPHJJPBATY6ECJ';
 
 const EventBrite = {
   search(term, address, date, sortBy) {
-    return fetch(`http://www.eventbriteapi.com/v3/events/search?token=2OHPMQAPHJJPBATY6ECJ&q=${term}&location.address=${address}&start_date.keyword=${date}&sort_by=${sortBy}`, {
+    return fetch(`https://cors-anywhere.herokuapp.com/http://www.eventbriteapi.com/v3/events/search?token=2OHPMQAPHJJPBATY6ECJ&q=${term}&location.address=${address}&start_date.keyword=${date}&sort_by=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${myToken}`
       }
@@ -11,11 +11,9 @@ const EventBrite = {
           return response.json();
         }).then(jsonResponse => {
       if (jsonResponse.events) {
-        return jsonResponse.events.map(event => ({
-          imageSrc: event.logo.original.url,
-          date: event.start.local,
-          name: event.name.text,
-          address: event.location.address
+        return jsonResponse.event.map(events => ({
+          date: events.start.local,
+          name: events.name.text,
         }));
       }
     });
